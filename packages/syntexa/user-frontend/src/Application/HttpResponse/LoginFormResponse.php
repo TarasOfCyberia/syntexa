d7b7e02e9279605da466ec07c7691a3d373dd9be
taras <taras.hanych@sapient.pro>
2025-11-05 16:10:18 +0200

    add AsHttpResponse attribute and enhance response handling with format and renderer support

--- packages/syntexa/user-frontend/src/Application/HttpResponse/LoginFormResponse.php
+++ packages/syntexa/user-frontend/src/Application/HttpResponse/LoginFormResponse.php
@@ -6,8 +6,9 @@ namespace Syntexa\UserFrontend\Application\HttpResponse;
 
 use Syntexa\Core\Http\Response\GenericResponse;
 use Syntexa\Core\Attributes\AsHttpResponse;
+use Syntexa\Core\Http\Response\ResponseFormat;
 
-#[AsHttpResponse(handle: 'login')]
+#[AsHttpResponse(handle: 'login', format: ResponseFormat::Layout)]
 class LoginFormResponse extends GenericResponse
 {
 }
